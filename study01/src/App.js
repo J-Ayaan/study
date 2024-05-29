e8328ff36b8f4bce0642cd635003e5e581ed0766
Ayaan-Jung <202221067@sangmyung.kr>
2024-05-29 16:18:12 +0900

    Revert "Merge pull request #2 from J-Ayaan/feature/#1-event"
    This reverts commit 4b9d4a244f055099dac80c67230030b348056424, reversing
    changes made to 7e143714cf9d0aa650106b569ebe50db220b9277.

--- study01/src/App.js
+++ study01/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [input, setinput] = useState("")
-  const onChange = (e) => setinput(e.target.value)
+function App() {
   return (
-    <>  
-      <input 
-        onChange={onChange}
-        placeholder="Input"
-        value={input}
-      />
-      {input}  
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
